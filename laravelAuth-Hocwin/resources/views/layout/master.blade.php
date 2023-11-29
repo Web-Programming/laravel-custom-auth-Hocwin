b2b2ff24a10e6162096a05632b32176764834613
Hocwin <146694692+Hocwin@users.noreply.github.com>
2023-11-29 13:04:22 +0700

    Policy

---
+++ laravelAuth-Hocwin/resources/views/layout/master.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>@yield('title')</title>
+</head>
+<body>
+    {{-- <h1>{{$kampus}}</h1>
+    <hr>
+
+    @yield('content')
+
+    <hr>
+     &copy; {{date('Y')}} Universitas Multi Data Palembang --}}
+
+     <div class="container">
+        @yield('content')
+
+        <hr>
+        &copy; {{date('Y')}} Universitas Multi Data Palembang
+     </div>
+</body>
+</html>
---
+++ laravelAuth-Hocwin/resources/views/prodi/index.blade.php
@@ -0,0 +1,40 @@
+@extends('layout')
+@section('content')
+<h2>Program Studi</h2>
+<div class="row pt-4">
+    <div class="col">
+        <h2>Prodi</h2>
+        <div class="d-md-flex justify-content-md-end">
+            <a href="{{route('prodi.create')}}" class="btn btn-primary">Tambah</a>
+        </div>
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Logo</th>
+                    <th>Nama</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($prodis as $item)
+                <tr>
+                    <td><img src="{{asset('storage/' .$item->foto)}}" width="100px" alt=""></td>
+                    <td>{{$item->nama}}</td>
+                     <td>
+                        <form action="{{ route('prodi.destroy', ['prodi' => $item->id])}}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <a href="{{ url('/prodi/' .$item->id)}}" class="btn btn-warning">Detail</a>
+                            <a href="{{ url('/prodi/' .$item->id. '/edit')}}" class="btn btn-info">Ubah</a>
+                            @can('delete', $item)
+                            <button type="submit" class="btn btn-danger">Hapus</button>
+                            @endcan
+                        </form>
+                     </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
